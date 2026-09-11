96b427dcb232c07d8418e0a1e1b5ed2677ef9882
Wanas24 <mwanas724@gmail.com>
2026-09-11 22:04:26 +0300

    edits removed

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -7,31 +7,28 @@ export async function proxy(request: NextRequest) {
   });
 
   const supabase = createServerClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    cookies: {
-      getAll() {
-        return request.cookies.getAll();
-      },
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
 
-      setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value, options }) => {
-          request.cookies.set(name, value);
-          response.cookies.set(name, value, options);
-        });
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            request.cookies.set(name, value);
+            response.cookies.set(name, value, options);
+          });
+        },
       },
     },
-  },
-);
+  );
 
-console.log("BEFORE GET USER");
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-const {
-  data: { user },
-} = await supabase.auth.getUser();
-
-console.log("AFTER GET USER", !!user);
   const isProtectedRoute = request.nextUrl.pathname.startsWith("/project");
 
   if (isProtectedRoute && !user) {
