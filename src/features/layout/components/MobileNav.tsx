a2dfbed59d4d28ce44374a94b7046f5a946ba058
Wanas24 <mwanas724@gmail.com>
2026-09-18 03:29:52 +0300

    Feat: mobile nav added

--- src/features/layout/components/MobileNav.tsx
+++ src/features/layout/components/MobileNav.tsx
@@ -1,11 +1,45 @@
-import MobileNavLinks from "./MobileNavLinks"
+import Image from "next/image";
+
+import { projectLinks } from "../data/projectLinks";
+import ProjectsIcon from "@/assets/icons/Projects";
 
 function MobileNav() {
   return (
-    <aside className=" hidden max-sm:absolute max-sm:block w-full  bottom-0 z-40">
-        <MobileNavLinks/>
+    <aside className="hidden max-sm:absolute max-sm:bottom-0 max-sm:z-40 max-sm:block w-full">
+      <div className="flex items-center justify-between gap-6 bg-surface-low py-4 px-6">
+        {projectLinks.slice(0, 2).map((link) => (
+          <div
+            key={link.href}
+            className="flex flex-col opacity-70 gap-0.5 items-center justify-center"
+          >
+            <Image src={link.icon} alt={link.label} />
+            <label htmlFor="" className="text-[10px] text-slate-dark">
+              {link.label}
+            </label>
+          </div>
+        ))}
+
+        <div className="flex flex-col opacity-70 gap-0.5 items-center justify-center">
+          <ProjectsIcon className="shrink-0" />
+          <label htmlFor="" className="text-[10px] text-slate-dark">
+            Projects
+          </label>
+        </div>
+
+        {projectLinks.slice(2, 4).map((link) => (
+          <div
+            key={link.href}
+            className="flex flex-col opacity-70 gap-0.5 items-center justify-center"
+          >
+            <Image src={link.icon} alt={link.label} />
+            <label htmlFor="" className="text-[10px] text-slate-dark">
+              {link.label}
+            </label>
+          </div>
+        ))}
+      </div>
     </aside>
-  )
+  );
 }
 
-export default MobileNav
+export default MobileNav;
